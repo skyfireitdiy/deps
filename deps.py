9f4e53ef535085278bda3f1421cf9ffb353384cc
skyfire <skyfireitdiy@hotmail.com>
2023-10-14 07:23:23 +0000

    优化实现

--- deps.py
+++ deps.py
@@ -8,46 +8,52 @@ import shutil
 
 
 def get_deps(elf):
-    output = subprocess.check_output("ldd " + elf, shell=True).decode()
+    output = subprocess.check_output(f"ldd {elf}", shell=True).decode()
     ld = ""
     libs = {}
     for line in output.splitlines():
-        if "=>" in line:
+        if "ld-linux" in line:
+            ld = line.split()[0]
+        elif "=>" in line:
             cols = line.split("=>")
             target = cols[1].split("(")[0].strip()
             libs[cols[0].strip()] = target
-        elif "ld-linux" in line:
-            ld = line.split()[0]
     return (ld, libs)
 
 
 def resolve_deps(elf, deps, work_dir):
     libs = deps[1]
     lib_dir = os.path.join(work_dir, "lib")
+    print(f"mkdir {lib_dir}")
     os.makedirs(lib_dir, exist_ok=True)
     for k, v in libs.items():
         realpath = os.path.realpath(v)
         if realpath == "":
             print(v, "not found!")
             sys.exit(0)
+
+        print(f"copy {realpath} => {os.path.join(lib_dir, k)}")
         shutil.copy(realpath, os.path.join(lib_dir, k))
     loader_name = os.path.basename(deps[0])
     loader_path = os.path.join(lib_dir, loader_name)
+    print(f"copy {deps[0]} => {loader_path}")
     shutil.copy(deps[0], loader_path)
 
     bin_dir = os.path.join(work_dir, "bin")
+    print(f"mkdir {bin_dir}")
     os.makedirs(bin_dir, exist_ok=True)
     elf_name = os.path.basename(elf)
+    print(f"copy {elf} => {os.path.join(bin_dir, elf_name)}")
     shutil.copy(elf, os.path.join(bin_dir, elf_name))
     start_script = os.path.join(work_dir, elf_name)
 
+    print(f"write start script to {start_script}")
     with open(start_script, "w") as fp:
         fp.write(
-            '''#!/bin/bash
+            f'''#!/bin/bash
 DIR=$(dirname $(realpath $0))
-$DIR/lib/%s --library-path $DIR/lib $DIR/bin/%s $*
-''' % (loader_name, elf_name)
-        )
+$DIR/lib/{loader_name} --library-path $DIR/lib $DIR/bin/{elf_name} $*
+''')
     os.chmod(start_script, 0o755)
 
 
@@ -56,8 +62,14 @@ def main():
     parser.add_argument("elf_path")
     parser.add_argument("-d", "--directory", required=True, help="工作路径")
     result = parser.parse_args()
-    deps = get_deps(result.elf_path)
-    resolve_deps(result.elf_path, deps, result.directory)
+    elf = subprocess.check_output(
+        "which " + result.elf_path, shell=True).decode().strip()
+    if not elf:
+        print(f"{elf} not found!")
+        exit()
+    deps = get_deps(elf)
+    resolve_deps(elf, deps, result.directory)
+    print("finished")
 
 
 if __name__ == "__main__":
